e76515f31471a5af4f5a5d529d0af2f41e8fbc50
Chinnayya Naidu Nalla <chinnayya.nalla@dtworld.ae>
2023-10-03 16:10:31 +0400

    Test Csae

--- tests/_base.spec.js
+++ tests/_base.spec.js
@@ -1,4 +1,4 @@
-const { request} = require('pactum');
+const { request, mock } = require('pactum');
 
 
 function setRequestDefaults() {
@@ -8,9 +8,27 @@ function setRequestDefaults() {
 
 before(async () => {
     console.log('hello');
+    mock.start();
+    mock.useRemoteServer("http://localhost:9393");
     setRequestDefaults();
+
 })
 
 after(async () => {
     console.log('bye');
 })
+
+mock.addInteraction({
+    request: {
+        method: 'GET',
+        path: '/status/418'
+    },
+    response: {
+        status: 418,
+        body: {
+            zipcode: 90210,
+            city: 'Beverly Hills'
+        }
+    }
+});
+
